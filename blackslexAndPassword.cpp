0c01ea7b5c34b5f7d54ec51006a85f4361958535
apexsindh <apexsindhoora@gmail.com>
2026-01-12 04:44:43 +0530

    blackslex and passward problem

---
+++ blackslexAndPassword.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,k,x,n;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>k>>x;
+        n=(k*x)+1;
+        cout<<n<<endl;
+    }
+    return 0;
+}
