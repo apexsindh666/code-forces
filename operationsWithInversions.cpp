8d1c38b40c38e258088fe6b7d34c761107b02a0d
apexsindh <apexsindhoora@gmail.com>
2026-01-16 18:02:56 +0530

    operations with inversions problem

--- operationsWithInversions.cpp
+++ operationsWithInversions.cpp
@@ -1,3 +1,16 @@
+// Given an array 𝑎1,𝑎2,…,𝑎𝑛. In one operation, you can choose a pair of indices 𝑖,𝑗 such that 1≤𝑖<𝑗≤𝑛, 𝑎𝑖>𝑎𝑗, and remove the element at index 𝑗 from the array. After that, the size of the array will decrease by 1, and the relative order of the elements will not change.
+
+// Determine the maximum number of operations that can be performed on the array if they are applied optimally.
+
+// Input
+// Each test contains multiple test cases. The first line contains the number of test cases 𝑡 (1≤𝑡≤50). The description of the test cases follows.
+
+// The first line of each test case contains an integer 𝑛 (1≤𝑛≤100) — the size of the initial array.
+
+// The second line of each test case contains 𝑛 natural numbers 𝑎1,𝑎2,…,𝑎𝑛 (1≤𝑎𝑖≤𝑛).
+
+// Output
+// For each test case, output the maximum number of operations that you can perform on the given array.
 #include<iostream>
 #include<vector>
 using namespace std;
