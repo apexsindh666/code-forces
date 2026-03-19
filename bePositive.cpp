cacfac0739b86ae2f71e6488672beb964b6ee24c
apexsindh <apexsindhoora@gmail.com>
2026-03-19 22:09:03 +0530

    be positive problem

---
+++ bePositive.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int neg = 0, zero = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+
+            if (x == -1) neg++;
+            else if (x == 0) zero++;
+        }
+
+        int ans = zero;
+
+        if (neg % 2 == 1) {
+            ans += 2;
+        }
+
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
